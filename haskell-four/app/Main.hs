c1d045436e815f48d714f3efece5fc12d87adca8
Nikita Korotkih <nkor@hellofresh.com>
2018-12-11 18:40:02 +0100

    haskell-four impl

--- haskell-four/app/Main.hs
+++ haskell-four/app/Main.hs
@@ -10,21 +10,24 @@ In Haskell data and functions to work with data are separated.
 --}
 
 class Printable p where
-    -- TODO
+    printMe :: p -> String
 
-data Foo = Foo | Bar
+data Foo = Foo | Bar | Int
 
 instance Printable Foo where
-    -- TODO
+    printMe Foo = "it's foo"
+    printMe Bar = "barrrr"
 
 helloP :: Printable p => p -> String
-helloP = undefined
+helloP p = "hi there " ++ printMe p
 
 {--
 ### excercise
 implement Printable instance for Int.
 Hint: check `show` function
 --}
+instance Printable Int where
+    printMe p = "it was int" ++ show p
 
 {--
 George Wilson - Type Class: The Ultimate Ad Hoc - https://www.youtube.com/watch?v=2EdQFCP5mZ8
@@ -41,35 +44,60 @@ Lets explore kinds of types with
 
 class Box b where -- b :: * -> *
     box :: a -> b a
+    -- box :: a -> a
     unbox :: b a -> a
 
 -- implement instance for Either
+-- data Either a b = Left a | Right b
+
+instance (Show e) => Box (Either e) where
+    box = Right
+
+    unbox (Right a) = a
+    unbox (Left b) = error $ show b
+
 
 {--
 ### excercise
 implement Box instance for Maybe
 --}
+-- data Maybe a = Nothing | Just a
+instance Box Maybe where
+    box = Just
+
+    unbox (Just a) = a
+    unbox Nothing = error "cannot extract value from nothing"
 
 
 {--
 ## Functor
 --}
 
-class Functor' f where
-    -- TODO
+class Functor' f where -- f :: * -> *
+    fmap' :: (a -> b) -> f a -> f b
 
 -- implement Functor for Maybe
+
+instance Functor' Maybe where
+    fmap' f (Just a) = Just $ f a
+    fmap' _ Nothing = Nothing
 
 {--
 ### excercise
 implement functor instance for Identity
 --}
 
-newtype Identity a = Identity a
+newtype Identity a = Identity a deriving (Show)
 
+instance Functor' Identity where
+    fmap' f (Identity a) = Identity $ f a
 
 -- TODO implement Reader functor
+newtype Reader e a = Reader (e -> a)
 
+instance Functor' (Reader e) where
+    -- (a -> b)    (e -> a)    (e -> b)
+    fmap' f (Reader g) = Reader $ f . g
 
 
 {--
