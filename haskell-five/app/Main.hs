f6b845d0becd936bcc066690309a1d725c1fdbea
Nikita Korotkih <nkor@hellofresh.com>
2018-12-18 18:29:07 +0100

    add impl for 5

--- haskell-five/app/Main.hs
+++ haskell-five/app/Main.hs
@@ -13,6 +13,7 @@ import Control.Monad
     Reader e a:     a (e->) b           (a, e) -> b         a -> e -> b         a -> (e -> b)
     State s a:      a (s->) b           (a, s) -> (b, s)    a -> s -> (b, s)    a -> (s -> (b, s))
     [a]:            a (i->) b           a -> [b]
+    Either e b:     a (f->) b           a -> Either e b
 
             +-------------------------------+
             |            a -> m b           |
@@ -33,7 +34,12 @@ import Control.Monad
 --}
 
 fishOperator :: (a -> [b]) -> (b -> [c]) -> a -> [c]
-fishOperator f g a = undefined
+fishOperator f g a =
+    let
+        bs = f a
+        css = fmap g bs
+    in
+        concat css
 
 {--
 ### We also need an equivalent for `id` - so:
@@ -52,30 +58,67 @@ fishOperator f g a = undefined
 {--
 ## So what is technically a monad in Haskell?
 --}
-
--- TBD Monad type class
+--     m :: * -> *
+-- class Monad m where
+--     return :: a -> m a
+--     (>>=) :: m a -> (a -> m b) -> m b
 
 -- TBD monad instance for Maybe'
+
+data Maybe' a = Nothing' | Just' a
+
+instance Functor Maybe' where
+    fmap f (Just' a) = Just' $ f a
+    fmap _ _ = Nothing'
+
+instance Applicative Maybe' where
+    pure = Just'
+
+    Just' f <*> Just' a = Just' $ f a
+    _ <*> _ = Nothing'
+
+
+instance Monad Maybe' where
+    return = Just'
+
+--   (>>=) :: m a -> (a -> m b) -> m b
+    (>>=) m f = case m of
+        Nothing' -> Nothing'
+        Just' x -> f x
 
 {--
 ## An example of monadic calculation with maybe
 --}
 
 checkRange :: Int -> Maybe Int
-checkRange x = undefined
+checkRange x =
+    if x > 10
+    then Nothing
+    else Just x
 
 safeDiv :: Int -> Int -> Maybe Int
-safeDiv x y = undefined
+safeDiv x y =
+    if y == 0
+    then Nothing
+    else Just $ x `div` y
 
 monadicValue :: Maybe (Int, Int, Int)
-monadicValue = undefined
+monadicValue = checkRange 11
+    >>= \a -> safeDiv 10 100
+    >>= \b -> checkRange 1
+    >>= \c -> return (a, b, c)
 
 {--
 ## Do notation as a syntactic sugar
 --}
 
 monadicValueWithDo :: Maybe (Int, Int, Int)
-monadicValueWithDo = undefined
+monadicValueWithDo = do
+    a <- checkRange 11
+    b <- safeDiv 10 100
+    c <- checkRange 1
+
+    return (a, b, c)
 
 
 
